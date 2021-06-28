416aa00c910105e5616ee92f8d6132b60262a111
DhruvGodambe <dhruvrockstone@gmail.com>
2021-06-28 10:05:51 +0530

    inserted git ignore

--- migrations/1_vesting_migration.js
+++ migrations/1_vesting_migration.js
@@ -5,7 +5,7 @@ module.exports = async function (deployer) {
   await deployer.deploy(Vesting);
   const vestingInstance = await Vesting.deployed();
   await deployer.deploy(Vesting_Factory, vestingInstance.address);
-  const vestingFactoryInstance = await Vesting_Factory.deployed();
-
+  
+  // const vestingFactoryInstance = await Vesting_Factory.deployed();
   // console.log("vesting instance", vestingInstance);
 };
