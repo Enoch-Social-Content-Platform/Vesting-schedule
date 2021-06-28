48b1631638c6c80f3df0b851aaf3de09d1555242
DhruvGodambe <dhruvrockstone@gmail.com>
2021-06-28 23:21:48 +0530

    removed comment

--- migrations/1_vesting_migration.js
+++ migrations/1_vesting_migration.js
@@ -4,7 +4,6 @@ const Vesting_Factory = artifacts.require("Vesting_Factory");
 module.exports = async function (deployer, network, accounts) {
   const start = Math.ceil(Date.now()/1000);
   await deployer.deploy(Vesting);
-  // , accounts[1], start, 3*24*60*60, start + 6*24*60*60, true
   const vestingInstance = await Vesting.deployed();
   await deployer.deploy(Vesting_Factory, vestingInstance.address);
   
